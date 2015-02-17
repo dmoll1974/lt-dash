d86d67f79a6c3623bdb7c68f4f2c543a1ffb1e31
Daniel Moll <daniel.moll@klm.com>
2015-02-17 22:17:22 +0100

    experimental stuff

--- app/controllers/dashboards.server.controller.js
+++ app/controllers/dashboards.server.controller.js
@@ -6,32 +6,43 @@
 var mongoose = require('mongoose'),
 	errorHandler = require('./errors.server.controller'),
 	Dashboard = mongoose.model('Dashboard'),
+    Product = mongoose.model('Product'),
 	_ = require('lodash');
 
+
+function getProductName(productId, callback){
+    
+    Product.findOne({_id: productId}).exec(function(err, product){
+        if (err) return next(err);
+        if (! product) return new Error('Failed to load ProductId ' + productId);
+        callback(product.name)
+        
+    });
+    
+}
 /**
  * Create a Dashboard
  */
 exports.create = function(req, res) {
 	var dashboard = new Dashboard(req.body);
 	dashboard.user = req.user;
-
-	dashboard.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(dashboard);
-		}
-	});
+    
+    getProductName(dashboard.productId, function(productName){
+        
+        dashboard.productName = productName;
+        
+        dashboard.save(function(err) {
+            if (err) {
+                return res.status(400).send({
+                    message: errorHandler.getErrorMessage(err)
+                });
+            } else {
+                res.jsonp(dashboard);
+            }
+        });
+    });
 };
 
-/**
- * Show the current Dashboard
- */
-exports.read = function(req, res) {
-	res.jsonp(req.dashboard);
-};
 
 /**
  * Update a Dashboard
@@ -85,6 +96,18 @@ exports.list = function(req, res) {
 };
 
 /**
+ * Show the current Dashboard
+ */
+exports.read = function(req, res) {
+    Dashboard.findOne({productId: req.product._id, name: req.params.dashboardName}).exec(function(err, dashboard){
+        if (err) return next(err);
+        if (! dashboard) return next(new Error('Failed to load Dashboard ' + id));
+        res.jsonp(dashboard);
+    })
+
+};
+
+/**
  * Dashboard middleware
  */
 exports.dashboardByID = function(req, res, next, id) { 
@@ -96,11 +119,11 @@ exports.dashboardByID = function(req, res, next, id) {
 	});
 };
 
-exports.dashboardByProductId = function(req, res, next, productId) {
-    Dashboard.find({productId: productId}).populate('metrics').exec(function(err, dashboards) {
+exports.dashboardByProductName = function(req, res, next, productName) {
+    Product.findOne({name: productName}).exec(function(err, product) {
         if (err) return next(err);
-        if (! dashboards) return next(new Error('Failed to load Dashboard ' + id));
-        req.dashboards = dashboards ;
+        if (! product) return next(new Error('Failed to load Product ' + productName));
+        req.product = product ;
         next();
     });
 };

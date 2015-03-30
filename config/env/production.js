afb93fcdce6a007300ea8f86ed61057942116c33
Daniel Moll <daniel.moll@klm.com>
2015-03-30 14:15:16 +0200

    added minified resources to boost performance (hopefully)

--- config/env/production.js
+++ config/env/production.js
@@ -23,31 +23,36 @@ module.exports = {
     assets: {
         lib: {
             css: [
-                'public/css/bootstrap.css',
+                //'public/css/bootstrap.css',
                 'public/css/style.css',
                 'public/lib/font-awesome/css/font-awesome.css',
 
-                'public/lib/bootstrap/dist/css/bootstrap.css',
-                'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-                'public/lib/ng-table/dist/ng-table.css',
-                'public/lib/ng-tags-input/ng-tags-input.css'
+                'public/lib/bootstrap/dist/css/bootstrap.min.css',
+                'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+                'public/lib/angular-material/angular-material.min.css',
+                //'public/lib/ng-table/dist/ng-table.css',
+                'public/lib/ng-tags-input/ng-tags-input.min.css',
+                'public/lib/angular-ui-bootstrap-datetimepicker/datetimepicker.css'
             ],
             js: [
-                'public/lib/angular/angular.js',
-                'public/lib/angular-resource/angular-resource.js',
-                'public/lib/angular-ui-router/release/angular-ui-router.js',
-                'public/lib/angular-ui-utils/ui-utils.js',
-                'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-                'public/lib/ng-table/dist/ng-table.js',
-                'public/lib/ng-tags-input/ng-tags-input.js',
-                'public/lib/underscore/underscore.js',
-                'public/lib/angular-modal-service/dst/angular-modal-service.min.js',
+                'public/lib/angular/angular.min.js',
+                'public/lib/angular-resource/angular-resource.min.js',
+                'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+                'public/lib/angular-ui-utils/ui-utils.min.js',
+                'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+                'public/lib/angular-animate/angular-animate.min.js',
+                'public/lib/angular-aria/angular-aria.min.js',
+                //'public/lib/angular-material/angular-material.min.js',
+                //'public/lib/ng-table/dist/ng-table.js',
+                'public/lib/ng-tags-input/ng-tags-input.min.js',
+                'public/lib/underscore/underscore-min.js',
+                //'public/lib/angular-modal-service/dst/angular-modal-service.min.js',
                 //'public/lib/jquery/dist/jquery.js',
                 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
                 'public/lib/highstock-release/highstock.js',
                 //'http://code.highcharts.com/stock/highstock.js',
-                'public/lib/highcharts-ng/dist/highcharts-ng.js',
-                //'https://rawgit.com/pablojim/highcharts-ng/master/src/highcharts-ng.js'
+                'public/lib/highcharts-ng/dist/highcharts-ng.min.js',
+                'public/lib/angular-ui-bootstrap-datetimepicker/datetimepicker.js'
 
             ]
         },

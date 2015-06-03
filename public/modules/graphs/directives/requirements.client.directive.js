47c73080c38be240f91cbe0d15a08f3a4317d123
Daniel Moll <dmoll1974@gmail.com>
2015-06-03 19:34:16 +0200

    tried to add tabs in requirements view

--- public/modules/graphs/directives/requirements.client.directive.js
+++ public/modules/graphs/directives/requirements.client.directive.js
@@ -33,62 +33,97 @@
 
         ) {
 
+            /* set tab number based on testRunMeetsRequirements */
 
-            TestRuns.getTestRunById($stateParams.productName, $stateParams.dashboardName, $stateParams.testRunId).success(function (testRun) {
+            $scope.tabNumber = TestRuns.selected.testrunMeetsRequirement ? 0 : 1;
 
-                TestRuns.selected = testRun;
+            $scope.setTab = function(newValue){
+                $scope.tabNumber = newValue;
+                if (newValue === 0) {
+                    $scope.showPassedRequirements = true;
+                }else{
+                    $scope.showPassedRequirements = false;
+                }
+                setTimeout(function(){
+                    $scope.tableParams.orderBy({});
+                    $scope.tableParams.reload();
 
-                var data = [];
+                }, 3000)
 
-                _.each(testRun.metrics, function(metric){
+            };
 
-                    /* only show metrics that fail to meet requirements */
-                    if(metric.metricMeetsRequirement === false) {
-
-                        var tag = (metric.tags) ? metric.tags[0].text : 'All';
-
-                        _.each(metric.targets, function (target) {
+            $scope.isSet = function(tabNumber){
+                return $scope.tabNumber  === tabNumber;
+            };
 
 
+            $scope.$watch('showPassedRequirements', function (newVal, oldVal) {
 
-                            data.push({
+//                    if (newVal !== oldVal) {
 
-                                target: target.target,
-                                value: target.value,
-                                targetMeetsRequirement: target.targetMeetsRequirement,
-                                metric: metric.alias,
-                                metricId: metric._id,
-                                requirementOperator: metric.requirementOperator,
-                                requirementValue: metric.requirementValue,
-                                testRunId: testRun.testRunId,
-                                productName: $stateParams.productName,
-                                dashboardName: $stateParams.dashboardName,
-                                tag: tag
+
+                        TestRuns.getTestRunById($stateParams.productName, $stateParams.dashboardName, $stateParams.testRunId).success(function (testRun) {
+
+                            TestRuns.selected = testRun;
+                            var showPassedRequirements = $scope.showPassedRequirements || TestRuns.selected.testrunMeetsRequirement;
+
+                            var data = [];
+
+
+                            _.each(testRun.metrics, function (metric) {
+
+                                /* only show metrics have requirements */
+                                if (metric.metricMeetsRequirement === showPassedRequirements ) {
+
+                                    var tag = (metric.tags) ? metric.tags[0].text : 'All';
+
+                                    _.each(metric.targets, function (target) {
+
+
+                                        data.push({
+
+                                            target: target.target,
+                                            value: target.value,
+                                            targetMeetsRequirement: target.targetMeetsRequirement,
+                                            metric: metric.alias,
+                                            metricId: metric._id,
+                                            requirementOperator: metric.requirementOperator,
+                                            requirementValue: metric.requirementValue,
+                                            testRunId: testRun.testRunId,
+                                            productName: $stateParams.productName,
+                                            dashboardName: $stateParams.dashboardName,
+                                            tag: tag
+
+                                        });
+                                    });
+                                }
+                            });
+
+
+
+                            $scope.tableParams = new ngTableParams({
+                                page: 1,            // show first page
+                                count: 50          // count per page
+
+                            }, {
+                                groupBy: 'metric',
+                                total: data.length,
+                                getData: function($defer, params) {
+                                    var orderedData = params.sorting() ?
+                                        $filter('orderBy')(data, $scope.tableParams.orderBy()) :
+                                        data;
+
+                                    $defer.resolve(orderedData);
+                                }
+                            });
+
 
                         });
-                        })
-                    }
-                })
 
+//                    }
 
-                $scope.tableParams = new ngTableParams({
-                    page: 1,            // show first page
-                    count: 50          // count per page
-
-                }, {
-                    groupBy: 'metric',
-                    total: data.length,
-                    getData: function($defer, params) {
-                        var orderedData = params.sorting() ?
-                            $filter('orderBy')(data, $scope.tableParams.orderBy()) :
-                            data;
-
-                        $defer.resolve(orderedData);
-                    }
-                });
-            });
-
-
+                }
+            );
 
         }
     }

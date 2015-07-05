8fed29fd35ffb80de562c2be9de7eaf182a1605a
x090371 <Daniel.Moll@klm.com>
2015-07-05 22:17:56 +0200

    added export db

--- app/controllers/import-db.server.controller.js
+++ app/controllers/import-db.server.controller.js
@@ -25,161 +25,189 @@ function upload (req, res) {
 
     };
 
+    /* remove existing collections */
 
-    _.each(products, function(importProduct){
+    Event.remove({}, function(err) {
+        if (err) console.log(err);
+        console.log('Events removed')
 
-        var newProduct = new Product();
+        Metric.remove({}, function(err) {
+            if (err) console.log(err);
+            console.log('Metrics removed')
 
-        newProduct.name = importProduct.name;
-        newProduct.description = importProduct.description;
-        newProduct.dashboards = importProduct.dashboards;
+            Dashboard.remove({}, function(err) {
+                if (err) console.log(err);
+                console.log('Dashoards removed')
+                Product.remove({}, function(err) {
+                    if (err) console.log(err);
+                    console.log('Products removed')
 
-        newProduct.save(function (err, NewProduct) {
-            if (err) return console.error(err);
+                    _.each(products, function(importProduct){
 
-            var importProductDashboards = [];
-            _.each(dashboards, function(dashboard){
+                        var newProduct = new Product();
 
-                if (_.indexOf(importProduct.dashboards, dashboard._id)!= -1 )
-                    importProductDashboards.push(dashboard);
+                        newProduct.name = importProduct.name;
+                        newProduct.description = importProduct.description;
+                        newProduct.dashboards = importProduct.dashboards;
 
-            });
+                        newProduct.save(function (err, NewProduct) {
+                            if (err) return console.error(err);
 
-            _.each(importProductDashboards, function(importDashboard){
+                            var importProductDashboards = [];
+                            _.each(dashboards, function(dashboard){
 
-                var newDashboard = new Dashboard();
-                var dashboardName = importDashboard.name.split('-');
-                newDashboard.productId = newProduct._id;
-                newDashboard.name = dashboardName[1];
-                newDashboard.description = importDashboard.description;
-                newDashboard.metrics = importDashboard.metrics;
-                newDashboard.granularity = importDashboard.granularity;
-                newDashboard.metricsRegexWily = importDashboard.metricsRegexWily;
-                newDashboard.hosts = importDashboard.hosts;
-                newDashboard.applications = importDashboard.applications;
-                newDashboard.instances = importDashboard.instances;
-                //newDashboard.tags = importDashboard.tags;
-                //
-                newDashboard.save(function(err,newDashboard){
-                    if(err) console.log(err);
-                    if(newDashboard){
-                        var importDashboardMetrics = [];
-                        _.each(metrics, function(metric){
+                                if (_.indexOf(importProduct.dashboards, dashboard._id)!= -1 )
+                                    importProductDashboards.push(dashboard);
 
-                            if (_.indexOf(importDashboard.metrics, metric._id)!= -1 )
-                                importDashboardMetrics.push(metric);
+                            });
 
-                        });
+                            _.each(importProductDashboards, function(importDashboard){
 
-                        _.each(importDashboardMetrics, function(importDashboardMetric) {
+                                var newDashboard = new Dashboard();
+                                var dashboardName = importDashboard.name.split('-');
+                                newDashboard.productId = newProduct._id;
+                                newDashboard.name = dashboardName[1];
+                                newDashboard.description = importDashboard.description;
+                                newDashboard.metrics = importDashboard.metrics;
+                                newDashboard.granularity = importDashboard.granularity;
+                                newDashboard.metricsRegexWily = importDashboard.metricsRegexWily;
+                                newDashboard.hosts = importDashboard.hosts;
+                                newDashboard.applications = importDashboard.applications;
+                                newDashboard.instances = importDashboard.instances;
+                                //newDashboard.tags = importDashboard.tags;
+                                //
+                                newDashboard.save(function(err,newDashboard){
+                                    if(err) console.log(err);
+                                    if(newDashboard){
+                                        var importDashboardMetrics = [];
+                                        _.each(metrics, function(metric){
 
-                            var newMetric = new Metric();
-                            var tags = [];
-                            tags.push({text: importDashboardMetric.tag});
-                            newMetric.dashboardId = newDashboard._id;
-                            newMetric.dashboardName = newDashboard.name;
-                            newMetric.productName = newProduct.name;
-                            newMetric.alias = importDashboardMetric.alias;
-                            newMetric.targets = importDashboardMetric.target;
-                            newMetric.benchmarkWarning = importDashboardMetric.benchmarkWarning;
-                            newMetric.benchmarkIssue = importDashboardMetric.benchmarkIssue;
-                            newMetric.requirementValue = importDashboardMetric.requirementValue;
-                            newMetric.requirementOperator = importDashboardMetric.requirementOperator;
-                            newMetric.tags = tags;
-                                newMetric.type = importDashboardMetric.type;
+                                            if (_.indexOf(importDashboard.metrics, metric._id)!= -1 )
+                                                importDashboardMetrics.push(metric);
 
-                            newMetric.save(function (err, newMetric) {
+                                        });
+
+                                        _.each(importDashboardMetrics, function(importDashboardMetric) {
+
+                                            var newMetric = new Metric();
+                                            var tags = [];
+                                            tags.push({text: importDashboardMetric.tag});
+                                            newMetric.dashboardId = newDashboard._id;
+                                            newMetric.dashboardName = newDashboard.name;
+                                            newMetric.productName = newProduct.name;
+                                            newMetric.alias = importDashboardMetric.alias;
+                                            newMetric.targets = importDashboardMetric.target;
+                                            newMetric.benchmarkWarning = importDashboardMetric.benchmarkWarning;
+                                            newMetric.benchmarkIssue = importDashboardMetric.benchmarkIssue;
+                                            newMetric.requirementValue = importDashboardMetric.requirementValue;
+                                            newMetric.requirementOperator = importDashboardMetric.requirementOperator;
+                                            newMetric.tags = tags;
+                                            newMetric.type = importDashboardMetric.type;
+
+                                            newMetric.save(function (err, newMetric) {
+
+                                            });
+
+                                        });
+                                    }
+                                });
 
                             });
 
                         });
-                    }
+                    });
+
+
+                    _.each(events, function(importEvent){
+
+                        var event = new Event();
+
+                        var splitDashboardName = importEvent.dashboardName.split('-');
+
+                        var eventDesciption;
+
+                        switch(importEvent.eventDescription){
+
+                            case 'Start-loadtest':
+                                eventDesciption = 'start';
+                                break;
+                            case 'End-loadtest':
+                                eventDesciption = 'end';
+                                break;
+                            default:
+                                eventDesciption = importEvent.eventDescription;
+
+                        }
+                        event.eventTimestamp = importEvent.timestamp;
+                        event.productName = splitDashboardName[0];
+                        event.dashboardName = splitDashboardName[1];
+                        event.testRunId = importEvent.testRunId;
+                        event.eventDescription = eventDesciption;
+                        event.baseline = importEvent.baseline;
+                        event.buildResultKey = importEvent.buildResultKey;
+
+                        event.save(function (err) {
+                        });
+
+                    });
+
                 });
 
             });
-
         });
     });
 
 
-    _.each(events, function(importEvent){
 
-        var event = new Event();
 
-        var splitDashboardName = importEvent.dashboardName.split('-');
 
-        var eventDesciption;
 
-        switch(importEvent.eventDescription){
-
-            case 'Start-loadtest':
-                eventDesciption = 'start';
-                break;
-            case 'End-loadtest':
-                eventDesciption = 'end';
-                break;
-            default:
-                eventDesciption = importEvent.eventDescription;
-
-        }
-        event.eventTimestamp = importEvent.timestamp;
-        event.productName = splitDashboardName[0];
-        event.dashboardName = splitDashboardName[1];
-        event.testRunId = importEvent.testRunId;
-        event.eventDescription = eventDesciption;
-        event.baseline = importEvent.baseline;
-        event.buildResultKey = importEvent.buildResultKey;
-
-        event.save(function (err) {
-        });
-
-    });
 
     /* clean db */
 
 
-    setTimeout(function(){
-
-
-        Dashboard.find().exec(function(err, dashboards){
-
-            var activeDashboards = [];
-
-            _.each(dashboards, function(dashboard){
-
-                activeDashboards.push(dashboard._id.toString());
-            })
-
-            Product.find().exec(function(err, products){
-
-                var productId;
-
-                _.each(products, function(product){
-
-                    productId = product.toObject()._id.toString();
-                    var updatedDashboards = [];
-
-                    _.each(product.dashboards, function(dashboardId){
-
-                        if(_.indexOf(activeDashboards, dashboardId.toString()) !== -1)updatedDashboards.push(dashboardId)
-                    })
-
-                    Product.findOne({ _id: productId}, function (err, updateProduct) {
-
-                        updateProduct.dashboards = updatedDashboards;
-                        updateProduct.save(function(err, updateProduct){
-
-                        });
-                    })
-
-
-
-                })
-            });
-            res.redirect('/#!');
-        })
-
-    }, 30000);
+    //setTimeout(function(){
+    //
+    //
+    //    Dashboard.find().exec(function(err, dashboards){
+    //
+    //        var activeDashboards = [];
+    //
+    //        _.each(dashboards, function(dashboard){
+    //
+    //            activeDashboards.push(dashboard._id.toString());
+    //        })
+    //
+    //        Product.find().exec(function(err, products){
+    //
+    //            var productId;
+    //
+    //            _.each(products, function(product){
+    //
+    //                productId = product.toObject()._id.toString();
+    //                var updatedDashboards = [];
+    //
+    //                _.each(product.dashboards, function(dashboardId){
+    //
+    //                    if(_.indexOf(activeDashboards, dashboardId.toString()) !== -1)updatedDashboards.push(dashboardId)
+    //                })
+    //
+    //                Product.findOne({ _id: productId}, function (err, updateProduct) {
+    //
+    //                    updateProduct.dashboards = updatedDashboards;
+    //                    updateProduct.save(function(err, updateProduct){
+    //
+    //                    });
+    //                })
+    //
+    //
+    //
+    //            })
+    //        });
+    //        res.redirect('/#!');
+    //    })
+    //
+    //}, 30000);
 
 
 
